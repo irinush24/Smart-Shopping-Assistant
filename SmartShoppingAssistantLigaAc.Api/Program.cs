d00f67121fcb4e9ce81ec15cc14ffd7279150bb2
Vlad Dan <vlad.dan@netrom2015.onmicrosoft.com>
2026-04-21 14:32:18 +0300

    Migration

--- SmartShoppingAssistantLigaAc.Api/Program.cs
+++ SmartShoppingAssistantLigaAc.Api/Program.cs
@@ -1,3 +1,7 @@
+
+using Microsoft.EntityFrameworkCore;
+using SmartShoppingAssistantLigaAc.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var connectionString = builder.Configuration.GetConnectionString("SmartShoppingAssistantContext");
+
+builder.Services.AddDbContext<SmartShoppingAssistantDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
